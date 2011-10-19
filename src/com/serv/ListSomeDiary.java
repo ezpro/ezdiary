35f81441fc83fe2696c4ce4ccf6bf6be99f0b7b6
vimous <gjyaiya@126.com>
2011-10-19 21:36:55 +0800

    .

--- src/com/serv/ListSomeDiary.java
+++ src/com/serv/ListSomeDiary.java
@@ -1,4 +1,4 @@
-package serv;
+package com.serv;
 
 public class ListSomeDiary {
 
--- src/com/serv/Login.java
+++ src/com/serv/Login.java
@@ -1,4 +1,4 @@
-package serv;
+package com.serv;
 
 public class Login extends EZServlet{
 	public Login(String user,String pass){
